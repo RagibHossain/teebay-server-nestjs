31dfbbb9919786b0c95090745d8e2edaa88dbc39
RagibIbneHossain <ragibibnehossain@gmail.com>
2024-05-13 03:48:58 +0600

    feat(resource): removed user resource

--- src/user/dto/user-login.dto.ts
+++
@@ -1,9 +0,0 @@
-export class LoginUserDTO {
-    readonly id: number;
-    readonly firstName: string;
-    readonly lastName: string;
-    readonly address: string;
-    readonly email: string;
-    readonly phoneNumber: string;
-    readonly password: string;
-}
